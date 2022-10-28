c1059bbdd68efc1670d2c91c8ed251b3353c27e9
saltgossoon <96102343+saltgossoon@users.noreply.github.com>
2022-10-29 01:19:34 +0800

    Update net_info.js

--- PanelScripts/net_info.js
+++ PanelScripts/net_info.js
@@ -29,7 +29,7 @@ $httpClient.get('http://ip-api.com/json/?lang=en', function (error, response, da
     $done({
       title:rootName,
       content:
-		`国家地区: ${jsonData.country} - ${jsonData.city}\n`+
+		`国家&地区: ${jsonData.country} - ${jsonData.city}\n`+
       `运营商 : ${jsonData.isp}\n` +
 		`数据中心: ${jsonData.org}`,
       icon: params.icon,
